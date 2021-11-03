375a26dfc70f14b16fecaf22c9d5aa63bbe1ca97
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-03 13:28:10 -0700

    Add totalingScore - Tests 3, 4, and 6: Passed.
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,21 +12,55 @@ PlayerScore.prototype.scoring = function() {
   if (this.currentRoll > 1) {
     return this.turnTotal += this.currentRoll;
   } else if (this.currentRoll === 1) {
-    turnTotal = 0;
+    this.turnTotal = 0;
     // end turn
   }
 }
 
+PlayerScore.prototype.totalingScore = function() {
+  this.totalScore += this.turnTotal;
+}
+
+
 let newroll = new PlayerScore();
 newroll.roll();
 newroll.scoring();
+newroll.totalingScore();
+console.log(newroll);
 newroll.roll();
 newroll.scoring();
+newroll.totalingScore();
+console.log(newroll);
 newroll.roll();
 newroll.scoring();
+newroll.totalingScore();
+console.log(newroll);
 newroll.roll();
 newroll.scoring();
+newroll.totalingScore();
+console.log(newroll);
 newroll.roll();
 newroll.scoring();
+newroll.totalingScore();
+console.log(newroll);
 newroll.roll();
 newroll.scoring();
+newroll.totalingScore();
+console.log(newroll);
+newroll.roll();
+newroll.scoring();
+newroll.totalingScore();
+console.log(newroll);
+
+
+// playerTurn {
+//   playercounter = 0;
+//   if (playercounter === 0) {
+//     // player one's turn
+//     playercounter += 1;
+//   }
+//   if (playercounter === 1) {
+//     // player two's turn
+//     playercounter -= 1;
+//   }
+// }
