036fd3c46bf7ff0a7610527a1aa1c5a71904511f
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-03 11:42:22 -0700

    Add currentRoll to turnTotal in scoring function.
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function PlayerScore(currentRoll, turnTotal, totalScore) {
   this.currentRoll = currentRoll
-  this.turnTotal = turnTotal;
-  this.totalScore = totalScore;
+  this.turnTotal = 0;
+  this.totalScore = 0;
 }
 
 PlayerScore.prototype.roll = function() {
@@ -10,7 +10,7 @@ PlayerScore.prototype.roll = function() {
 
 PlayerScore.prototype.scoring = function() {
   if (this.currentRoll > 1) {
-    return this.currentRoll; 
+    return this.turnTotal += this.currentRoll;
   } else if (this.currentRoll === 1) {
     turnTotal = 0;
     // end turn
@@ -18,5 +18,15 @@ PlayerScore.prototype.scoring = function() {
 }
 
 let newroll = new PlayerScore();
+newroll.roll();
+newroll.scoring();
+newroll.roll();
+newroll.scoring();
+newroll.roll();
+newroll.scoring();
+newroll.roll();
+newroll.scoring();
+newroll.roll();
+newroll.scoring();
 newroll.roll();
 newroll.scoring();
